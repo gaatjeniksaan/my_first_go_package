f670bff30ef38158ac563ed496f7b5743a89d8d6
barendlinders <barendlinders@gmail.com>
2018-11-13 21:46:34 +0100

    update package

--- package_one.go
+++ package_one.go
@@ -1,19 +1,25 @@
 package trial
 
-type Person {
+import "fmt"
+
+type Person struct {
 	firstname string
-	lastname string
-	age int
+	lastname  string
+	age       int
 }
 
 func (p *Person) AddAge(toAdd int) {
 	p.age = p.age + toAdd
 }
 
-func (p *Person) ChangeFirstName(p newFirstName) {
+func (p *Person) ChangeFirstName(newFirstName string) {
 	p.firstname = newFirstName
 }
 
-func (p *Person) ChangeLastName(p newLastName) {
+func (p *Person) ChangeLastName(newLastName string) {
 	p.lastname = newLastName
+}
+
+func (p Person) PrintName() {
+	fmt.Println("Hello I am ", p.firstname, p.lastname)
 }
